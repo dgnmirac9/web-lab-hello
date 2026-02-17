9cabc28d9027c8ed02a9773c6e76c5b21ba987fa
Miraç Doğan <165315433+dgnmirac9@users.noreply.github.com>
2026-02-17 11:25:04 +0300

    feat: add personal details to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
       <h2>LAB-1</h2>
       <p>Ad Soyad: Mirac Doğan</p>
       <p>Ogrenci No: 220541018</p>
+      <p>Teknoloji fakültesi Yazılım Mühendisliği 3.sınıf öğrencisiyim. Web Tasarım ve Programlama dersi kapsamında Vite, React, npm, JavaScript, Node.js gibi kavramları öğrenmiş olduk.</p>
+      <p>Bu dönemki hedeflerim:</p>
+      <p>1-Veri Yapıları, Otomata, Diferansiyel Denklemler ve Veri Madenciliği derslerini AA ile vermek</p>
+      <p>2-LoL'de diamond olmak.</p>
     </div>
   ) 
 }
