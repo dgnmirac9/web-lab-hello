49b2a29cbafcf4f4e44d548076766249e63dd2ca
Miraç Doğan <165315433+dgnmirac9@users.noreply.github.com>
2026-02-17 11:16:14 +0300

    chore: initial project setup  with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return(
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Mirac Doğan</p>
+      <p>Ogrenci No: 220541018</p>
+    </div>
+  ) 
+}
+export default App
